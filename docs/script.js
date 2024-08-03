0bc918531c2ea7e71f168aa8414812b018da25e7
Alif2609 <105056689+Alif2609@users.noreply.github.com>
2024-08-04 02:54:08 +0700

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $framework = $(".framework");
+    $container = $(".html");
+
+    // $container.click(function(){
+    //     $framework.toggle(200);
+    // });
+    
+});
+
